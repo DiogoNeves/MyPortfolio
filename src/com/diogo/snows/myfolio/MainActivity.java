7b1ea0ffc5240b8a7b35082ae4fc372f3b582fb6
Diogo Neves <diogo.neves@gmail.com>
2012-08-21 03:26:37 +0100

    Created a simple contact screen

--- src/com/diogo/snows/myfolio/MainActivity.java
+++ src/com/diogo/snows/myfolio/MainActivity.java
@@ -1,68 +1,73 @@
 package com.diogo.snows.myfolio;
 
+import java.io.IOException;
+
+import android.content.res.AssetManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
-import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.ImageView;
 
 public class MainActivity extends FragmentActivity {
 
-    /**
-     * The {@link android.support.v4.view.PagerAdapter} that will provide fragments for each of the
-     * sections. We use a {@link android.support.v4.app.FragmentPagerAdapter} derivative, which will
-     * keep every loaded fragment in memory. If this becomes too memory intensive, it may be best
-     * to switch to a {@link android.support.v4.app.FragmentStatePagerAdapter}.
-     */
-    SectionsPagerAdapter mSectionsPagerAdapter;
+	/**
+	 * The {@link android.support.v4.view.PagerAdapter} that will provide
+	 * fragments for each of the sections. We use a
+	 * {@link android.support.v4.app.FragmentPagerAdapter} derivative, which
+	 * will keep every loaded fragment in memory. If this becomes too memory
+	 * intensive, it may be best to switch to a
+	 * {@link android.support.v4.app.FragmentStatePagerAdapter}.
+	 */
+	SectionsPagerAdapter mSectionsPagerAdapter;
 
-    /**
-     * The {@link ViewPager} that will host the section contents.
-     */
-    ViewPager mViewPager;
+	/**
+	 * The {@link ViewPager} that will host the section contents.
+	 */
+	ViewPager mViewPager;
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        // Create the adapter that will return a fragment for each of the three primary sections
-        // of the app.
-        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		// Create the adapter that will return a fragment for each of the three
+		// primary sections
+		// of the app.
+		mSectionsPagerAdapter = new SectionsPagerAdapter(
+				getSupportFragmentManager());
 
+		// Set up the ViewPager with the sections adapter.
+		mViewPager = (ViewPager) findViewById(R.id.pager);
+		mViewPager.setAdapter(mSectionsPagerAdapter);
 
-        // Set up the ViewPager with the sections adapter.
-        mViewPager = (ViewPager) findViewById(R.id.pager);
-        mViewPager.setAdapter(mSectionsPagerAdapter);
+	}
 
-    }
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.activity_main, menu);
+		// return true;
+		return false;
+	}
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_main, menu);
-        return true;
-    }
+	/**
+	 * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
+	 * one of the primary sections of the app.
+	 */
+	public class SectionsPagerAdapter extends FragmentPagerAdapter {
 
-    
+		private final class SectionInfo {
+			private final String mTitle;
+			private final Fragment mFragment;
 
-
-    /**
-     * A {@link FragmentPagerAdapter} that returns a fragment corresponding to one of the primary
-     * sections of the app.
-     */
-    public class SectionsPagerAdapter extends FragmentPagerAdapter {
-    	
-    	private final class SectionInfo {
-    		private final String mTitle;
-    		private final Fragment mFragment;
-    		
-    		/**
+			/**
 			 * @return the mTitle
 			 */
 			public String getTitle() {
@@ -77,56 +82,62 @@ public class MainActivity extends FragmentActivity {
 			}
 
 			public SectionInfo(String title, Fragment fragment) {
-    			mTitle = title;
-    			mFragment = fragment;
-    		}
-    	}
-    	
-    	private SectionInfo[] mSections;
+				mTitle = title;
+				mFragment = fragment;
+			}
+		}
 
-        public SectionsPagerAdapter(FragmentManager fm) {
-            super(fm);
-            
-            mSections = new SectionInfo[] { new SectionInfo("contact", new DummySectionFragment()) };
-        }
+		private SectionInfo[] mSections;
 
-        @Override
-        public Fragment getItem(int i) {
-        	Fragment fragment = mSections[i].getFragment();
-            Bundle args = new Bundle();
-            args.putInt(DummySectionFragment.ARG_SECTION_NUMBER, i + 1);
-            fragment.setArguments(args);
-            return fragment;
-        }
+		public SectionsPagerAdapter(FragmentManager fm) {
+			super(fm);
 
-        @Override
-        public int getCount() {
-            return mSections.length;
-        }
+			mSections = new SectionInfo[] { new SectionInfo("contact",
+					new ContactSectionFragment()) };
+		}
 
-        @Override
-        public CharSequence getPageTitle(int position) {
-            return mSections[position].getTitle();
-        }
-    }
+		@Override
+		public Fragment getItem(int i) {
+			return mSections[i].getFragment();
+		}
 
-    /**
-     * A dummy fragment representing a section of the app, but that simply displays dummy text.
-     */
-    public static class DummySectionFragment extends Fragment {
-        public DummySectionFragment() {
-        }
+		@Override
+		public int getCount() {
+			return mSections.length;
+		}
 
-        public static final String ARG_SECTION_NUMBER = "section_number";
+		@Override
+		public CharSequence getPageTitle(int position) {
+			return mSections[position].getTitle();
+		}
+	}
 
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                Bundle savedInstanceState) {
-            TextView textView = new TextView(getActivity());
-            textView.setGravity(Gravity.CENTER);
-            Bundle args = getArguments();
-            textView.setText(Integer.toString(args.getInt(ARG_SECTION_NUMBER)));
-            return textView;
-        }
-    }
+	/**
+	 * A dummy fragment representing a section of the app, but that simply
+	 * displays dummy text.
+	 */
+	public static class ContactSectionFragment extends Fragment {
+		public ContactSectionFragment() {
+		}
+
+		@Override
+		public View onCreateView(LayoutInflater inflater, ViewGroup container,
+				Bundle savedInstanceState) {
+			View contactView = inflater.inflate(R.layout.contact_layout, null);
+
+			ImageView qrCode = (ImageView) contactView
+					.findViewById(R.id.qrcode);
+			if (qrCode != null) {
+				try {
+					AssetManager assetManager = getActivity().getAssets();
+					Bitmap qrImage = BitmapFactory.decodeStream(assetManager
+							.open("qrcode.png"));
+					qrCode.setImageBitmap(qrImage);
+				} catch (IOException e) {
+				}
+			}
+
+			return contactView;
+		}
+	}
 }
